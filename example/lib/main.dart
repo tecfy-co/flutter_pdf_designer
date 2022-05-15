9f2343a3b8edd7ab79b2764409720af62c8c4944
Ahmed Galal <agalal819@gmail.com>
2022-05-15 21:49:52 +0200

    Enhance Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
+//First import flutter_pdf_designer
 import 'package:flutter_pdf_designer/flutter_pdf_designer.dart';
+// Second import data_model
 import 'package:flutter_pdf_designer/src/data_model.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
    MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //declare dataModel as late to initialize lately at initState function and
+  late DataModel dataModel;
+  // initialize List of Elements
+  List<Elements>? elements = [
+  Elements(
+  type: WidgetType.text,
+  text: 'Tecfy.co',
+  color: 0xffFF000000,
+  fontSize: 20.0),];
+
+  @override
+  void initState() {
+    // initialize dataModel
+    dataModel = DataModel(width: 200,height: 200,elements:elements);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +41,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: PdfDesign(onChange: (json){},json:{"width":300.0,"height":300.0,
-          'elements':[{'type':WidgetType.text,'text':"Galal",'fontSize':30.0,'xPo'
-              'sition':0.0,'yPosition':0.0}
-          ]},width: 100,height: 200,),
+        //call PdfDesign in your widget tree, pass to it width and height to
+        // make box of printing box's size
+        body: PdfDesign(onChange: (json){},json: dataModel.toJson(),width: 100,
+          height: 200,),
       ),
     );
   }
